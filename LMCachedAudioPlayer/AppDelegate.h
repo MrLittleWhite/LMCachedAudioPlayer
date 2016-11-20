afb02f42b4b204893e5b761dee685eee56ebd740
lazy-iOS2 <luffy243077002@163.com>
2016-11-20 18:08:49 +0800

    Initial Commit

---
+++ LMCachedAudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMCachedAudioPlayer
+//
+//  Created by lazy-iOS2 on 16/11/20.
+//  Copyright © 2016年 lazy-iOS2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
